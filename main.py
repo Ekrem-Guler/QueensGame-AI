548a3d46a7bb3fda7e96192899eb8598a2a6ca49
Ekrem <ekremg006@gmail.com>
2025-10-16 20:48:07 +0300

    I deleted some unnecessary parts and I added some descriptions. The game is ready.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import copy
 game_size = 9
 table_arr = []
 color_arr = []
-
+player_game = []
 
 def in_a_row(m,number):
     if(number==0):
@@ -62,7 +62,6 @@ for i in range(game_size):
     arr = [0]*game_size
     arr[game_arr[i]] = i+10
     table_arr.append(arr)
-
 #############################################################################################
 
 # It is finding the left, right, up and down points of the given point.
@@ -87,8 +86,11 @@ colors_loc = []
 # colors_loc is needed for the store all colors rows and columns
 # side_arr is storing the all sides of each color. When a color moving a different square it will append new sides.
 # c_arr is store the history of chosen square for colors.
+# player_game is for backtracking algorithm and player. The game will be start from 0's.
 
 for i in range(game_size):
+    arr = [0] * game_size
+    player_game.append(arr)
     colors_loc.append([])
     side_arr.append(side(i,game_arr[i],game_size,[]))
     c_arr.append((i,game_arr[i]))
@@ -96,46 +98,42 @@ for i in range(game_size):
 m = 0
 for i in range(game_size):
     for j in range(game_size):
+
+# Every side can have same point. So, we are checking if it used before.
         for k in range(len(side_arr)):
             for l in side_arr[k]:
                 if (l in c_arr):
                     side_arr[k].remove(l)
+        # Get a random queen's point.
         choice = random.randint(0,game_size-1)
-
-
 
         m = 0
         null = False
         while(len(side_arr[choice])==0):
             m+=1
             choice = random.randint(0,game_size-1)
-
+            # It is just trying to reduce bug rate. If it loops game_size+10 it will say there is not a choice and check it for every queen's point.
             if(m==game_size+10):
-
                 for k in range(len(side_arr)):
                     if(len(side_arr[k]) != 0):
                         choice=k
-
                         if(len(side_arr[choice])==0):
                             continue
                         else:
                             break
-
                     else:
                         null = True
                 if(null):
                     break
         if(null):
             break
-
-
+# colored = point which is colored. colors_loc stores all point according to queen's row.
         colored= random.choice(side_arr[choice])
         c_arr.append(colored)
         table_arr[colored[0]][colored[1]] = choice+10
         colors_loc[choice].append((colored[0],colored[1]))
-
         side_arr[choice].remove(colored)
-
+# This will add side's side to choiced color sides.
         colored_arr = side(colored[0], colored[1], game_size,c_arr)
         for k in colored_arr:
             if (k in side_arr[choice] or k in c_arr):
@@ -143,30 +141,11 @@ for i in range(game_size):
             else:
                 side_arr[choice].append(k)
 
-# A set to keep track of elements that have been seen
-
-
-
+#CHECK the table.
 for i in range(len(table_arr)):
     print(table_arr[i])
 
-
-player_game = []
-player_hint_game= []
-columns_empty = []
-colors_empty = []
-for i in range(game_size):
-    arr = [0]*game_size
-    columns_empty.append(game_size)
-    player_hint_game.append(arr)
-    player_game.append(arr)
-    colors_empty.append(len(colors_loc[i]))
-rows_empty = columns_empty
-print(colors_empty)
-print(player_game)
-dup = player_game
-
-
+# Checks any mismovement.
 def win_or_not(choice_x,choice_y,table_arr,player_game):
         if(player_game[choice_x][choice_y] != 0):
             return False
@@ -185,9 +164,9 @@ def win_or_not(choice_x,choice_y,table_arr,player_game):
                 return False
         return True
 
+# Playing the game(USER)
 def game():
     while(True):
-        player_choice = 0
         game_over = 0
         says_empty =  int(input(f"if you mark cross write 0 if you mark a queen write 1: "))
         while(says_empty!=0 and says_empty!=1):
@@ -196,39 +175,20 @@ def game():
         x_coordinate = int(input(f"Choose a x coordinate 0-{game_size-1}: "))
         y_coordinate = int(input(f"Choose a y coordinate 0-{game_size-1}: "))
 
-        while (says_empty == 1 and win_or_not(x_coordinate, y_coordinate) == False):
-            says_empty = int(input(f"if you mark cross write 0 if you mark a queen write 1: "))
+        while (says_empty == 1 and win_or_not(x_coordinate, y_coordinate,table_arr,player_game) == False):
+            says_empty = int(input(f"PLease choose valid number. if you mark cross write 0 if you mark a queen write 1: "))
             x_coordinate = int(input(f"Please choose valid number. Choose a x coordinate 0-{game_size - 1}: "))
             y_coordinate = int(input(f"Please choose valid number. Please choose a y coordinate 0-{game_size - 1}: "))
 
-        if(says_empty==1):
-            player_choice = int(input(f"Color 0-{game_size-1}: "))
 
-            while(player_choice >game_size-1 or player_choice <0):
-                player_choice = int(input(f"Please choose valid number. Color 0-{game_size-1}: "))
         while(x_coordinate >game_size-1 or y_coordinate >game_size-1 or x_coordinate <0 or y_coordinate <0):
             x_coordinate = int(input(f"Please choose valid number. Choose a x coordinate 0-{game_size-1}: "))
             y_coordinate = int(input(f"Please choose valid number. Please choose a y coordinate 0-{game_size-1}: "))
 
 
         if(says_empty==1):
-            player_game[x_coordinate][y_coordinate] = player_choice+10
-            # player_hint_game[x_coordinate][y_coordinate] = player_choice+10
+            player_game[x_coordinate][y_coordinate] = 1001
             game_over+=1
-            # color = table_arr[x_coordinate][y_coordinate]-10
-            # sum_of_crosses = 0
-            # for j in range(len(colors_loc[color])):
-            #     if(colors_loc[color][j][0] != x_coordinate and colors_loc[color][j][1] != y_coordinate):
-            #         player_hint_game[colors_loc[color][j][0]][colors_loc[color][j][1]] = -1
-            #         sum_of_crosses += 1
-            # for j in range(game_size):
-            #     if (j != y_coordinate):
-            #         player_hint_game[x_coordinate][j] = -1
-            #         sum_of_crosses += 1
-            #     if(j != x_coordinate):
-            #         player_hint_game[j][y_coordinate] = -1
-            #         sum_of_crosses += 1
-            #
             if(game_over==9):
                 break
         else:
@@ -236,7 +196,7 @@ def game():
         # print(player_hint_game)
         print(player_game)
 
-
+# Finds corners.
 def sidess(i,rand):
     sides = [[i - 1, rand - 1], [i - 1, rand + 1], [i + 1, rand - 1], [i + 1, rand + 1]]
     sides_all=[]
@@ -246,18 +206,16 @@ def sidess(i,rand):
                 sides_all.append(k)
     return sides_all
 
+# Second and working backtracking try.
 def ai_try2(player_game):
     all_number = list(range(game_size))
     all =[]
     game = []
-    tried_rand = []
-    tried_rand.append([])
     answer = []
     i=-1
     count = 1
     passed=0
     back = 0
-    tried_rand = -1
     while(True):
         i+=1
         print(player_game)
@@ -268,11 +226,12 @@ def ai_try2(player_game):
         rand = all_number[randomm]
         tried = []
         tried.append(rand)
-
         we_stop = False
         print(all_number)
         print(rand)
         print(f"i: {i}")
+
+# Randomly choosing a queen column for first row if it can't pass the mismovement(win_or_not) then it will go back.
         while  win_or_not(i, rand,table_arr,player_game) == False:
             if rand not in tried:
                 tried.append(rand)
@@ -282,9 +241,11 @@ def ai_try2(player_game):
             if len(tried) == len(all_number):
                 we_stop = True
                 break
-
+# We didn't stop so, we can loop the while and -1 for the impossible points.
         if(we_stop == False):
+            # How many times we passed without any stop.
             passed+=1
+            # count is counting the backwards from the table.
             if(passed>=max(back,2)):
                 count = 1
             for k in sidess(i,rand):
@@ -304,6 +265,7 @@ def ai_try2(player_game):
             answer.append((i,rand))
             all_number.remove(rand)
 
+# We stopped for backward from the table. All history came back. and we will try new solution again.
         else:
             passed=0
             print(f"COUNT:> {count}")
@@ -332,27 +294,27 @@ def ai_try2(player_game):
             break
     print(player_game)
     return player_game
-
-
-answer = []
 players_game = ai_try2(player_game)
-for i in range(len(players_game)):
-    for j in range(len(players_game[i])):
-        if(players_game[i][j] == 1001):
+###################################### YOU CAN RECOMMENT TO FIND ANY DUPLICATE COLOR IN THE BACKTRACKING ANSWER ###############################################
+# answer = []
 
-            answer.append(table_arr[i][j])
-            print(table_arr[i][j])
-
-seen = set()
-# A list to store duplicates found in the input list
-duplicates = []
-
-# Iterate over each element in the list
-for i in answer:
-    if i in seen:
-        duplicates.append(i)
-    else:
-        seen.add(i)
-
-print(duplicates)
+# for i in range(len(players_game)):
+#     for j in range(len(players_game[i])):
+#         if(players_game[i][j] == 1001):
+#
+#             answer.append(table_arr[i][j])
+#             print(table_arr[i][j])
+#
+# seen = set()
+# # A list to store duplicates found in the input list
+# duplicates = []
+#
+# # Iterate over each element in the list
+# for i in answer:
+#     if i in seen:
+#         duplicates.append(i)
+#     else:
+#         seen.add(i)
+#
+# print(duplicates)
 print("Game Over")
